2125afed923fbe80888e08da65058a563782e545
trustwells <trust.wells@gmail.com>
2025-05-23 23:44:45 -0500

    updating server.c

--- server.c
+++ server.c
@@ -5,16 +5,42 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+#define PORT 12345
 
+int main()
+{
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[1024] = {0};
+    socklen_t addr_len = sizeof(client_addr);
 
-int main() {
+    // step 1 create a socket, if returning -1 then print error
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // 2. Bind to IP and port, use IPv4, any IP address, and convert host port to network version in binary (big endian)
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY; // 0.0.0.0
+    server_addr.sin_port = htons(PORT);
 
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // 3. Listen
+    if (listen(server_fd, 5) < 0)
+    {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
 
+    printf("Listening for a connection...");
+
+    // 4. Accept a connection
 }
-
-
-
-
-
